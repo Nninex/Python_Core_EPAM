07d550f8bf143ef364bb2a528ada6c155e276b0f
Nninex <118069914+Nninex@users.noreply.github.com>
2025-09-26 22:49:26 +0400

    Create oop2.py

---
+++ oop2.py
@@ -0,0 +1,40 @@
+'''
+OOP Basics. Task 2 
+ Description 
+Create a class SchoolMember which represents any person in school. Classes Teacher and Student are inherited from SchoolMember. 
+Classes should have the same interface with the public show () method. Teacher accepts name (str), age (int), salary (int). Student accepts name (str),
+age (int), grades. Move the same logic of initialization to the class SchoolMember. 
+Method show() returns string (see string patters in Example). 
+Example 
+>>> persons = [Teacher("Mr.Snape", 40, 3000), Student("Harry", 16, 75)]
+ >>> for person in persons:
+print(person.show())
+ "Name: Mr.Snape, Age: 40, Salary: 3000"
+"Name: Harry, Age: 16, Grades: 75"
+'''
+
+
+class SchoolMember:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+    
+    def show(self):
+        raise NotImplementedError("Subclasses must implement show()")
+            
+
+class Teacher(SchoolMember):
+    def __init__(self, name: str, age: int, salary: int):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def show(self):
+        return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}"
+
+class Student(SchoolMember):
+    def __init__(self, name: str, age: int, grades: int):
+        super().__init__(name, age)
+        self.grades = grades
+
+    def show(self):
+        return f"Name: {self.name}, Age: {self.age}, Grades: {self.grades}"
