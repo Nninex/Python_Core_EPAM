15c69278fd06379c7ad22d8bd12d7b4c43e441b2
Nninex <118069914+Nninex@users.noreply.github.com>
2025-09-26 22:48:14 +0400

    Create oop1.py

---
+++ oop1.py
@@ -0,0 +1,21 @@
+'''
+OOP Basics. Task 1 
+ Develop a class Field with “full encapsulation”, whose attributes are accessed and data changes are implemented through method calls. 
+In OOP, it is generally accepted to start the names of methods for extracting data with the word “get”, and the names of the methods in which fields are
+equated to a certain value - “set”. 
+In this task, you need to implement get_value and set_value methods for Field class (__value property)
+'''
+class Field:
+    __value = None
+    def __init__(self):
+        self.__value = None
+    
+    '''  `get_value` and `set_value` methods for `Field` class (`__value` property).'''
+    
+    def get_value(self):
+        return self.__value
+    
+    def set_value(self, value):
+        self.__value = value
+
+
