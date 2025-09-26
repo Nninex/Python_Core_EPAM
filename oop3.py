5995c3ea6e12b1f8325b3ddadb9dc14cc1be7a09
Nninex <118069914+Nninex@users.noreply.github.com>
2025-09-26 22:50:30 +0400

    Create oop3.py

---
+++ oop3.py
@@ -0,0 +1,22 @@
+'''
+OOP Basics. Task 3 
+Implement a Counter class that optionally accepts the start value and the counter stop value. If the start value is not specified the counter should begin
+with 0. If the stop value is not specified it should be counting up infinitely. If the counter reaches the stop value, print “Maximal value is reached.” 
+Implement two methods: “increment” and “get” 
+Example: ```python  c = Counter(start=42) c.increment() c.get() 43 
+c = Counter() c.increment() c.get() 1 c.increment() c.get() 2 
+c = Counter(start=42, stop=43) c.increment() c.get() 43 c.increment() Maximal value is reached. c.get() 43 ``
+'''
+class Counter:
+    def __init__(self, start: int = 0, stop: int = None):
+        self.value = start
+        self.stop = stop
+
+    def increment(self):
+        if self.stop is not None and self.value >= self.stop:
+            print("Maximal value is reached.")
+        else:
+            self.value += 1
+
+    def get(self):
+        return self.value
