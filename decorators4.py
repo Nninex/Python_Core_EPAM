a63345833b7c71a18a81d993055c5c1004e8008f
Nninex <118069914+Nninex@users.noreply.github.com>
2025-10-07 23:43:32 +0400

    Create decorators4.py

---
+++ decorators4.py
@@ -0,0 +1,26 @@
+'''
+Functions. Decorators. Decorators. Task 4. 
+Decorators Factory 
+Create a decorators factory (decorator itself). The factory accepts a function (lambda) as an input and returns a decorator that will return the result of
+the function as the first argument, the result of the decorated function is passed. The function which the factory accepts (in the example below it is a
+lambda) can take one positional parameter only. 
+For example: ```python 
+@decorator_apply(lambda user_id: user_id + 1) def return_user_id(num: int): return num return_user_id(42) 43
+'''
+
+def decorator_apply(lambda_func):
+    """
+    A decorator factory that applies `lambda_func` 
+    to the result of the decorated function.
+    """
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return lambda_func(result)
+        return wrapper
+    return decorator
+
+
+@decorator_apply(lambda user_id: user_id + 1)
+def return_user_id(num: int) ->int:
+    return num
